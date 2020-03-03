57e0baf94bdb815f2b17a1989b223655da100344
Gaurav Verma <gauravve1@gmail.com>
2020-03-03 14:44:57 +0530

    wishlist stash

--- client/src/components/OrderTile.jsx
+++ client/src/components/OrderTile.jsx
@@ -3,26 +3,32 @@ import React from "react";
 const OrderTile = ({ product, index }) => {
   return (
     <>
-      <div className="row alert alert-secondary border border-dark py-2">
-        <div className="col-1 d-flex   justify-content-center align-items-center">
-          {index + 1}
-        </div>
-        <div className="col-2 d-flex   justify-content-center align-items-center">
-          <img src={product.image} alt="prod-thumb" style={{ width: "100%" }} />
-        </div>
-        <div className="col-3 d-flex   justify-content-center align-items-center">
-          <small className="font-weight-bold">
-            {product.name.substr(0, 15)}...
-          </small>
-        </div>
-        <div className="col-2 d-flex justify-content-center align-items-center">
-          {product.price}
-        </div>
-        <div className="col-2 d-flex   justify-content-center align-items-center">
-          X {product.qty}
-        </div>
-        <div className="col-1 d-flex justify-content-center align-items-center">
-          {product.price * product.qty}
+      <div className="container">
+        <div className="row alert alert-secondary border border-dark py-2">
+          <div className="col-1 d-flex   justify-content-center align-items-center">
+            {index + 1}
+          </div>
+          <div className="col-2 d-flex   justify-content-center align-items-center">
+            <img
+              src={product.image}
+              alt="prod-thumb"
+              style={{ width: "100%" }}
+            />
+          </div>
+          <div className="col-3 d-flex   justify-content-center align-items-center">
+            <small className="font-weight-bold">
+              {product.name.substr(0, 15)}...
+            </small>
+          </div>
+          <div className="col-1 d-flex justify-content-center align-items-center">
+            {product.price}
+          </div>
+          <div className="col-3 d-flex   justify-content-center align-items-center">
+            X {product.qty}
+          </div>
+          <div className="col-1 d-flex justify-content-center align-items-center">
+            {product.price * product.qty}
+          </div>
         </div>
       </div>
     </>
--- client/src/components/Wishlist.jsx
+++ client/src/components/Wishlist.jsx
@@ -1,13 +1,31 @@
 import React, { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { fetchWishlist } from "../store/wishlist/actions/wishlist.actions";
+import AddToWishlist from "./AddToWishlist";
+import OrderTile from "./OrderTile";
 
 const Wishlist = () => {
   const dispatch = useDispatch();
   const wishlist = useSelector(state => state.wishlist);
   const token = useSelector(state => state.isLoggedIn.token);
 
-  return <div>Wishlist</div>;
+  return (
+    <>
+      <div className="container mt-5">
+        <h3>Wishlist</h3>
+        <div className="container">
+          <div className="row">
+            <div className="col-10">
+              {/* <OrderTile /> */}
+            </div>
+            <div className="col-2">
+              {/* <AddToWishlist /> */}
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Wishlist;
