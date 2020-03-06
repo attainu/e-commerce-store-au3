034d43d1a7829145dfe8f8227490095d7d2445cf
Gaurav Verma <gauravve1@gmail.com>
2020-03-06 19:59:52 +0530

    path import

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const bodyParser = require("body-parser");
-
+const path = require("path");
 require("dotenv").config();
 const PORT = process.env.PORT;
 
@@ -37,9 +37,6 @@ if (process.env.NODE_ENV === "production") {
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname + "/client/public/index.html"));
 });
-
-
-
 
 // Get Routes
 let product = require("./server/routes/products");
